ae4cc3b62510d4fb75c2a8df8e77e70211ba4509
Barkha-Yadav <barkhayadav1606@gmail.com>
2026-07-02 21:38:18 +0530

    opt: add another approach which takes the same O(N) but the simpler one than two pointers for Max Consecutive Ones

--- src/arrays/_485_MaxConsecutiveOnes.java
+++ src/arrays/_485_MaxConsecutiveOnes.java
@@ -1,14 +1,19 @@
 package arrays;
 
 // Leetcode 485: Max Consecutive Ones
-// Time Complexity : O(N)
-// Space Complexity : O(1)
+// Time Complexity : O(N) - for both approaches
+// Space Complexity : O(1) - for both approaches
 public class _485_MaxConsecutiveOnes {
     public static void main(String[] args) {
         int[] arr = {1,1,0,1,1,1};
 
         System.out.println("Max consecutive one's in the above array are: "+findMaxConsecutiveOnes(arr));
+
+        System.out.println("Max consecutive ones using second approach: "+findMaxConsecutiveOnes2(arr));
     }
+
+    // Approach: 1
+    // two pointer approach
     public static int findMaxConsecutiveOnes(int[] nums) {
         int l = 0;
         int r = 0;
@@ -27,5 +32,22 @@ public class _485_MaxConsecutiveOnes {
             }
         }
         return maxLen;
+    }
+
+    // Approach 2:
+    // a simpler for loop
+    public static int findMaxConsecutiveOnes2(int[] nums){
+        int count = 0;
+        int maxCount = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+                maxCount = Math.max(count,maxCount);
+            }
+            else{
+                count = 0;
+            }
+        }
+        return maxCount;
     }
 }
