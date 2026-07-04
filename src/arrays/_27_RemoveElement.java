d676579ed2b66bc20d43d3db54d532813e1924d3
Barkha-Yadav <barkhayadav1606@gmail.com>
2026-07-04 23:31:13 +0530

    opt: add O(N) optimal solution for Leetcode 27 Remove Element

--- src/arrays/_27_RemoveElement.java
+++ src/arrays/_27_RemoveElement.java
@@ -6,8 +6,14 @@ public class _27_RemoveElement {
         int val = 2;
 
         System.out.println("The number of elements in array except "+val+" in count are: "+removeElement(arr,val));
+
+        // because in java the arrays are pass by reference i.e the swap function we perform in approach 1 changes the value in the original array arr
+        int[] arr2 = {0,1,2,2,3,0,4,2};
+        System.out.println("Tune number of elements in array except "+val+" in count are: "+removeElement2(arr2,val));
     }
 
+    // Approach 1
+    // this approach includes unnecessary complexity to the code like swap and count
     // time complexity: O(N)
     // space complexity: O(1)
     public static int removeElement(int[] nums, int val) {
@@ -30,5 +36,20 @@ public class _27_RemoveElement {
             else break;
         }
         return count;
+    }
+
+    // Approach 2
+    // using fast and slow  pointer
+    // time complexity: O(N)
+    // space complexity: O(1)
+    public static int removeElement2(int[] nums, int val){
+        int k = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
     }
 }
