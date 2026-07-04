e188cde080631ae639a2c92010155d3257ff35e8
Barkha-Yadav <barkhayadav1606@gmail.com>
2026-07-04 23:20:42 +0530

    feat: add Leetcode: 27 Remove Element

---
+++ src/arrays/_27_RemoveElement.java
@@ -0,0 +1,34 @@
+package arrays;
+
+public class _27_RemoveElement {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,2,3,0,4,2};
+        int val = 2;
+
+        System.out.println("The number of elements in array except "+val+" in count are: "+removeElement(arr,val));
+    }
+
+    // time complexity: O(N)
+    // space complexity: O(1)
+    public static int removeElement(int[] nums, int val) {
+        int l = 0;
+        int r = nums.length-1;
+        while(l<=r){
+            if(nums[r] == val) r--;
+            else if(nums[l] == val){
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
+            }
+            else l++;
+        }
+
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != val) count++;
+            else break;
+        }
+        return count;
+    }
+}
