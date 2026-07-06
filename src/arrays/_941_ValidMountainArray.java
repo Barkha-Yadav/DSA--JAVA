275b1ff4d9d3f129706021db11d441e4a8981482
Barkha-Yadav <barkhayadav1606@gmail.com>
2026-07-06 23:41:47 +0530

    opt: add O(N) optimal solution for leetcode 941 Valid Mountain Array

--- src/arrays/_941_ValidMountainArray.java
+++ src/arrays/_941_ValidMountainArray.java
@@ -6,12 +6,18 @@ public class _941_ValidMountainArray {
     public static void main(String[] args) {
         int[] arr = {0,3,2,1};
         System.out.println(Arrays.toString(arr));
+
+        // Approach 1
         if(validMountainArray(arr)){
             System.out.println("Valid Mountain array");
         }
         else{
             System.out.println("Not a mountain array");
         }
+
+        // Approach 2
+        if(validMountainArray2(arr)) System.out.println("Valid Mountain Array");
+        else System.out.println("Not a mountain array");
     }
 
     // Approach 1
@@ -32,5 +38,26 @@ public class _941_ValidMountainArray {
             if(arr[j]<arr[j+1]) return false;
         }
         return true;
+    }
+
+    // Approach 2
+    // time complexity: O(N)
+    // space complexity: O(1)
+    public static boolean validMountainArray2(int[] arr){
+        if(arr.length<3) return false;
+
+        // think like 2 person climbing the mountain from both the ends and they are going to meet at the same point
+        int l = 0;
+        int r = arr.length-1;
+
+        while(l<arr.length-1 && arr[l]<arr[l+1]){
+            l++;
+        }
+
+        while(r>0 && arr[r]<arr[r-1]){
+            r--;
+        }
+
+        return l>0 && r<arr.length-1 && l==r;
     }
 }
