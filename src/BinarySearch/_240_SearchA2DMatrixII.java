4454de43f2fec66a946746f0d2c984f54128f683
Barkha-Yadav <barkhayadav1606@gmail.com>
2026-07-08 18:18:09 +0530

    feat: add leetcode 240: Search a 2D Matrix II

---
+++ src/BinarySearch/_240_SearchA2DMatrixII.java
@@ -0,0 +1,23 @@
+package BinarySearch;
+
+public class _240_SearchA2DMatrixII {
+    public static void main(String[] args) {
+        int[][] arr = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        int target = 21;
+
+        System.out.println(searchMatrix(arr,target));
+    }
+
+    // time complexity: O(m+n)
+    // space complexity: O(1)
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length-1;
+        while(row<matrix.length && col>=0){
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target) row++;
+            else col--;
+        }
+        return false;
+    }
+}
